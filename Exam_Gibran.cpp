eab0aa0bd13289dc2a6f4511b3320caede136dac
LuigiTan <gibran.garcia@uniat.edu.mx>
2023-03-23 10:42:13 -0600

    First commit

---
+++ Exam_Gibran.cpp
@@ -0,0 +1,11 @@
+// Exam_Gibran.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
+
